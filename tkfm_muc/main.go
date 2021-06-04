911313c55ce5f456429df98e187d24a4bfb3c83f
洛尹夏 <luoyinxia@protonmail.com>
2021-06-04 20:31:38 +0800

    refactor(tkfm_muc): refactored `main` function
    code new function `daysChecker` to handler user input data.

--- tkfm_muc/main.go
+++ tkfm_muc/main.go
@@ -6,20 +6,32 @@ func main() {
 	var days int
 	var demonites int = 0
 
+	// get user input data
 	fmt.Println("Please input you want to make up days:")
 	_, e := fmt.Scanln(&days)
-	if e != nil {
-		fmt.Println("Error: Days must be set to interger.")
-		return
-	}
-	if days <= 0 {
-		fmt.Println("Error: Make up days must be greater than '0'")
+	// error handlers
+	ok := daysChecker(days, e)
+	if !ok {
 		return
 	}
 
+	// calculate
 	for i := 0; i < days; i++ {
 		demonites += (50 + 5*i)
 	}
 
+	// show result
 	fmt.Printf("Ok, you shall spend '%d' Demonties.\n", demonites)
+}
+
+func daysChecker(days int, e error) bool {
+	if e != nil {
+		fmt.Println("Error: Days must be set to integer.")
+		return false
+	}
+	if days <= 0 {
+		fmt.Println("Error: Make up days must be greater than '0'")
+		return false
+	}
+	return true
 }
