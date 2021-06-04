cc5cf4248e9c7dfc0b61099f0e26be072275fd38
洛尹夏 <luoyinxia@protonmail.com>
2021-06-04 21:22:10 +0800

    feat(tkfm_muc): add compare conditions
    add compare conditions to `daysChecker` function,
    such as compare input days with days of current month.

--- tkfm_muc/main.go
+++ tkfm_muc/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	var days int
@@ -25,13 +28,44 @@ func main() {
 }
 
 func daysChecker(days int, e error) bool {
+	// default error handler
 	if e != nil {
 		fmt.Println("Error: Days must be set to integer.")
 		return false
 	}
+	// input days checker
 	if days <= 0 {
-		fmt.Println("Error: Make up days must be greater than '0'")
+		fmt.Println("Error: Make up days must be greater than '0'.")
 		return false
+	}
+	// compare input days with days of current month
+	currentYear := time.Now().Year()
+	currentMonth := time.Now().Month().String()
+	switch currentMonth {
+	case "January", "March", "May", "July", "August", "October", "December":
+		if days > 31 {
+			fmt.Println("Error: The current month only have 31 days.")
+			return false
+		}
+	case "April", "June", "September", "November":
+		if days > 30 {
+			fmt.Println("Error: The current month only have 30 days.")
+			return false
+		}
+	case "February":
+		// days of february(common year)
+		var daysOfFeb int = 28
+		// days of february(leap year)
+		if (currentYear%100 == 0) && (currentYear%400 == 0) {
+			daysOfFeb = 29
+		} else if (currentYear%100 != 0) && (currentYear%4 == 0) {
+			daysOfFeb = 29
+		}
+
+		if days > daysOfFeb {
+			fmt.Printf("Error: The current month only have %d days.\n", daysOfFeb)
+			return false
+		}
 	}
 	return true
 }
